daa67dd0707a4942df29a98b75ba7f78822919ee
Randy <randy@randyrants.com>
2007-11-21 19:01:09 +0000

    Added SharpMTExtension to the SharpMT SLN file Updated ExamplePlugIn and SharpMTPlugIn2 to use the Solution-based SharpMTExtension output Updated the Build Event for ExamplePlugIn and SharpMTPlugIn2 to be configuration independent Removed the VSInstall projects from all three SLN files Fixed a problem with resources that went missing in Pocket SharpMT Added blogExtension.dll to the ext directory for SharpMTPlugIn2

---
+++ SharpMTExtension/SharpMTExtension/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿#region Using directives
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+#endregion
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SharpMTExtension Interface")]
+[assembly: AssemblyDescription("Offline MT-based Blog writer plug-in interface")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("RandyRants.com")]
+[assembly: AssemblyProduct("SharpMTExtension")]
+[assembly: AssemblyCopyright("Copyright 2003-2006 RandyRants.com")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("3.2.1.0")]
